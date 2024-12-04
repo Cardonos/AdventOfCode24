4ae6761e2374ffb37fd3106daf197cada49a45b7
Cardonos <lasse@lasse-stausberg.de>
2024-12-04 18:00:00 +0100

    Day 4

--- Day3.py
+++ Day3.py
@@ -1,10 +1,8 @@
 import re
-
 data = open("Inputs/Day3.txt").read()
 
 #Part1
 match = re.findall("mul\(\d{1,3},\d{1,3}\)", data)
-
 result = 0
 for i in match:
     find_number = re.findall("\d{1,3}", i)
@@ -12,10 +10,7 @@ for i in match:
 print(result)
 
 #Part2
-
-
 cleaned_in = re.sub(r"don't\(\).*?(?:do\(\)|$)", "", data, flags=re.DOTALL)
-
 result_clean = 0
 match_do = re.findall("mul\(\d{1,3},\d{1,3}\)", cleaned_in)
 for j in match_do:
