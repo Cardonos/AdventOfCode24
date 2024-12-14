da614ae0d86346bccd9af17d6ecfc570314894bf
Cardonos <119761878+Cardonos@users.noreply.github.com>
2024-12-14 12:49:41 +0100

    Day 14 - empirical solution for part 2

--- Day14.py
+++ Day14.py
@@ -13,45 +13,17 @@ def move(coordinates, room, speed, seconds):
 
 def safety_factor(room, size):
     height, width = size
-    q1 = 0
-    q2 = 0
-    q3 = 0
-    q4 = 0
-    i = 0
-    while i < height/2-1:
-        j = 0
-        while j < width/2-1:
-            q1 += room[i][j]
-            j+=1
-        i += 1
-    i = 0
-    while i < height / 2 - 1:
-        j = round(width/2)+1
-        while j < width:
-            q2 += room[i][j]
-            j += 1
-        i += 1
-    i = round(height/2)
-    while i < height:
-        j = 0
-        while j < width / 2 - 1:
-            q3 += room[i][j]
-            j += 1
-        i += 1
-    i = round(height / 2)
-    while i < height:
-        j = round(width / 2)
-        while j < width:
-            q4 += room[i][j]
-            j += 1
-        i += 1
+    q1 = sum(sum(room[0:round(height/2)-1, 0:round(width/2)]))
+    q2 = sum(sum(room[0:round(height/2)-1,round(width/2)+1::]))
+    q3 = sum(sum(room[round(height/2)::, 0:round(width/2)-1]))
+    q4 = sum(sum(room[round(height/2)::,round(width/2)+1::]))
     return q1*q2*q3*q4
 
 
 def check_for_tree():
     t = 0
-    max_x = 0
-    max_y = 0
+    max_x = 4
+    max_y = 4
     bathroom = np.zeros([103,101])
     while True:
         for robot in robots:
@@ -61,31 +33,21 @@ def check_for_tree():
             v_y = int(robot[1].split(",")[1])
             end_x,end_y = move([x_coord,y_coord],np.shape(bathroom),[v_x,v_y],t)
             bathroom[end_y][end_x] += 1
-            '''
-            i = 0
-            while i < 103:
-                j = 0
-                while j < 101:
-                    if sum(sum(bathroom[i:i+10, j:j+5])) > 15:
-                        print("yep", t)
-                        print(bathroom[i:i+10, j:j+5])
-                    j += 5
-                i += 5
-            '''
+        #Check for the standard deviation of sum in x-direction and y-direction
+        #the picture will be unusually ordered, so when both standard deviations are at their maximum it is very
+        #likely it is the solution
         a = statistics.stdev(np.sum(bathroom,0))
         if a > max_x:
             max_x = a
         b = statistics.stdev(np.sum(bathroom,1))
         if b > max_y:
             max_y = b
-        if a >= 7.4663:
-            if b >= 6.83474:
+        if a >= max_x:
+            if b >= max_y:
                 print(f"Part 2: {t}")
                 return
         bathroom = np.zeros([103,101])
-
         t+=1
-    return
 
 bathroom = np.zeros([103,101])
 
@@ -103,4 +65,5 @@ for robot in robots:
 
 print(f"Part 1: {safety_factor(bathroom, np.shape(bathroom))}")
 
+#Part2
 check_for_tree()
